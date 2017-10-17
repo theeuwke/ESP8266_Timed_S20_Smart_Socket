a1232f16f5c94e8eb42df250f9c71854f1920c71
Bart Theeuwes <barthe@Bart-PC.lan>
2017-10-17 23:14:03 +0200

    * full implementation

--- ESP8266_Timed_S20_Smart_Socket.ino
+++ ESP8266_Timed_S20_Smart_Socket.ino
@@ -1,182 +1,242 @@
+//OTA
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
-#include <ESP8266WebServer.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-#include <Ticker.h>
-#include <EEPROM.h>
 
-#include "global.h"
-#include "NTP.h"
+//WiFiManager
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>   
 
-// Include the HTML, STYLE and Script "Pages"
-// Key?
+//Blynk
+#define BLYNK_PRINT Serial
 
-#include "Page_Admin.h"
-#include "Page_Script.js.h"
-#include "Page_Style.css.h"
-#include "Page_NTPSettings.h"
-#include "Page_Information.h"
-#include "Page_General.h"
-#include "Page_NetworkConfiguration.h"
+//Hardware
+#include <TimedBlink.h>
 
-extern "C" {
-#include "user_interface.h"
+#include <BlynkSimpleEsp8266.h>
+#include <TimeLib.h>
+#include <WidgetRTC.h>
+BlynkTimer  BlynkTijd;
+WidgetLED   BlynkStatusLed(V4);
+WidgetRTC   BlynkRtc;
+static int  BlynkTijdsDuur = 1;
+static int  BlynkUur = 18;
+static int  BlynkMinuut = 0;
+static int  BlynkSeconde = 0;
+static long BlynkCountDown = 0;
+static bool BlynkTimerArmed = false;
+
+//S20
+#include <eBtn.h>
+#define PIN_BUTTON       0
+#define PIN_BLUELEDRELAY 12
+#define PIN_GREENLED     13
+eBtn S20Button = eBtn(PIN_BUTTON);
+
+TimedBlink S20StatusLed(PIN_GREENLED);
+
+char auth[] = "YourAuthToken";
+
+void S20ButtonRelease() {
+  //S20 Button is ingedrukt, tijd verhogen met 1 uur, max 4 uur.
+  BlynkCountDown += (60 * 60); 
+  if(BlynkCountDown > (4 * 60 * 60))
+    BlynkCountDown = 4 * 60 * 60; 
 }
 
-Ticker ticker;
+void S20ButtonLong() {
+  BlynkCountDown = 0;
+}
 
-os_timer_t myTimer;
+void ESP8266PinISR(){
+  S20Button.handle();
+}
 
-//*** Normal code definition here ...
+//gets called when WiFiManager enters configuration mode
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("WiFiManager: Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+  //Led blink fast
+  S20StatusLed.blink(200,200);
+}
 
-#define LED_esp 2
+void BlynkTimerHandler(void) {
+  //DEBUG
+  String currentTime = String(hour()) + ":" + minute() + ":" + second();
+  String setTime = String(BlynkUur) + ":" + BlynkMinuut + ":" + BlynkSeconde;
 
-String chipID;
+  Serial.print("Current time: ");
+  Serial.println(currentTime);
 
-void setup() {
-  bool CFG_saved = false;
-  int WIFI_connected = false;
-  Serial.begin(115200);
-  
-   //**** Network Config load 
-  EEPROM.begin(512); // define an EEPROM space of 512Bytes to store data
-  CFG_saved = ReadConfig();
+  //1. Update LCD
+  String s = String("op tijdstip?: ");
+  if(BlynkTimerArmed)
+    s += "ja";
+  else
+    s += "nee";
+  s += "; " + setTime + "bezig: " + BlynkCountDown;
+  Blynk.virtualWrite(V3, s);
 
-  //  Connect to WiFi acess point or start as Acess point
-  if (CFG_saved)  //if no configuration yet saved, load defaults
-  {    
-      // Connect the ESP8266 to local WIFI network in Station mode
-      Serial.println("Booting");
-      //printConfig();
-      WiFi.mode(WIFI_STA);  
-      WiFi.begin(config.ssid.c_str(), config.password.c_str());
-      WIFI_connected = WiFi.waitForConnectResult();
-      if(WIFI_connected!= WL_CONNECTED )
-          Serial.println("Connection Failed! activating to AP mode...");
-      
-      Serial.print("Wifi ip:");Serial.println(WiFi.localIP());
+  //2. Controlleer of CountDown actief is
+  if(BlynkCountDown) {
+    digitalWrite(PIN_BLUELEDRELAY, HIGH);
+    BlynkStatusLed.on();
+  } else {
+    digitalWrite(PIN_BLUELEDRELAY, LOW);
+    BlynkStatusLed.off();
   }
 
-  if ( (WIFI_connected!= WL_CONNECTED) or !CFG_saved){
-    // DEFAULT CONFIG
-    Serial.println("Setting AP mode default parameters");
-    config.ssid = "ESP8266-" + String(ESP.getChipId(),HEX);       // SSID of access point
-    config.password = "" ;   // password of access point
-    config.dhcp = true;
-    config.IP[0] = 192; config.IP[1] = 168; config.IP[2] = 1; config.IP[3] = 100;
-    config.Netmask[0] = 255; config.Netmask[1] = 255; config.Netmask[2] = 255; config.Netmask[3] = 0;
-    config.Gateway[0] = 192; config.Gateway[1] = 168; config.Gateway[2] = 1; config.Gateway[3] = 254;
-    config.ntpServerName = "0.ch.pool.ntp.org"; // to be adjusted to PT ntp.ist.utl.pt
-    config.Update_Time_Via_NTP_Every =  5;
-    config.timeZone = 1;
-    config.isDayLightSaving = true;
-    config.DeviceName = "Not Named";
-    WiFi.mode(WIFI_AP);  
-    WiFi.softAP(config.ssid.c_str());
-    Serial.print("Wifi ip:");Serial.println(WiFi.softAPIP());
-     
-   }
-   
-    // Start HTTP Server for configuration
-    server.on ( "/", []() {
-      Serial.println("admin.html");
-      server.send_P ( 200, "text/html", PAGE_AdminMainPage);  // const char top of page
-    }  );
-  
-    server.on ( "/favicon.ico",   []() {
-      Serial.println("favicon.ico");
-      server.send( 200, "text/html", "" );
-    }  );
-  
-    // Network config
-    server.on ( "/config.html", send_network_configuration_html );
-    // Info Page
-    server.on ( "/info.html", []() {
-      Serial.println("info.html");
-      server.send_P ( 200, "text/html", PAGE_Information );
-    }  );
-    server.on ( "/ntp.html", send_NTP_configuration_html  );
-  
-    //server.on ( "/appl.html", send_application_configuration_html  );
-    server.on ( "/general.html", send_general_html  );
-    //  server.on ( "/example.html", []() { server.send_P ( 200, "text/html", PAGE_EXAMPLE );  } );
-    server.on ( "/style.css", []() {
-      Serial.println("style.css");
-      server.send_P ( 200, "text/plain", PAGE_Style_css );
-    } );
-    server.on ( "/microajax.js", []() {
-      Serial.println("microajax.js");
-      server.send_P ( 200, "text/plain", PAGE_microajax_js );
-    } );
-    server.on ( "/admin/values", send_network_configuration_values_html );
-    server.on ( "/admin/connectionstate", send_connection_state_values_html );
-    server.on ( "/admin/infovalues", send_information_values_html );
-    server.on ( "/admin/ntpvalues", send_NTP_configuration_values_html );
-    //server.on ( "/admin/applvalues", send_application_configuration_values_html );
-    server.on ( "/admin/generalvalues", send_general_configuration_values_html);
-    server.on ( "/admin/devicename",     send_devicename_value_html);
-  
-      server.onNotFound ( []() {
-      Serial.println("Page Not Found");
-      server.send ( 400, "text/html", "Page not Found" );
-    }  );
-    server.begin();
-    Serial.println( "HTTP server started" );
+  //3. Controlleer of er een Tijdschakeling actief is
+  if(BlynkTimerArmed) {
+    if((hour() == BlynkUur) && (minute() == BlynkMinuut) && (second() >= BlynkSeconde)) {
+      BlynkCountDown = (60 * 60) * BlynkTijdsDuur;
+      BlynkTimerArmed = false;
+    }
+  }
 
-    // ***********  OTA SETUP
-  
-    //ArduinoOTA.setHostname(host);
-    ArduinoOTA.onStart([]() { // what to do before OTA download insert code here
-        Serial.println("Start");
-      });
-      
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
-    
-    ArduinoOTA.onEnd([]() { // do a fancy thing with our board led at end
-        for (int i=0;i<30;i++){
-          analogWrite(LED_esp,(i*100) % 1001);
-          delay(50);
-        }
-        digitalWrite(LED_esp,HIGH); // Switch OFF ESP LED to save energy
-        Serial.println("\nEnd");
-        ESP.restart();
-      });
-  
-    ArduinoOTA.onError([](ota_error_t error) { 
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR) Serial.println("End Failed");
-        ESP.restart(); 
-      });
-  
-     /* setup the OTA server */
-    ArduinoOTA.begin();
-    Serial.println("Ready");
-      
-    // start internal time update ISR
-    tkSecond.attach(1, ISRsecondTick);
-        
-    //**** Normal Sketch code here...
+  //4. Update Led
+  int n;
+  n =  BlynkCountDown / 3600;
+  n += BlynkCountDown % 3600 ? 1 : 0;
+  switch(n) {
+    case 0:
+      S20StatusLed.blinkOff();
+    break;
+    case 1:
+      S20StatusLed.blink(50, 900);
+    break;
+    case 2:
+      S20StatusLed.blink(50, 450);
+    break;
+    case 3:
+      S20StatusLed.blink(50, 300);
+    break;
+    case 4:
+    default:
+      S20StatusLed.blink(50, 200);
+    break;
+  }
 }
 
-// the loop function runs over and over again forever
+void setup() {
+  //Serial
+  Serial.begin(115200);
+  Serial.println("Booting SmartDrankKast 1.0");
+
+  //S20 Hardware
+  pinMode(PIN_GREENLED, OUTPUT);
+  digitalWrite(PIN_GREENLED, HIGH);
+  pinMode(PIN_BLUELEDRELAY, OUTPUT);
+  digitalWrite(PIN_BLUELEDRELAY, LOW);
+  pinMode(PIN_BUTTON, INPUT);
+
+  //S20Button.on("press",pressFunc);
+  //S20Button.on("hold",holdFunc);
+  S20Button.on("release",S20ButtonRelease);
+  S20Button.on("long",S20ButtonLong);
+  //setting the interrupt on btn pin to react on change state
+  attachInterrupt(PIN_BUTTON, ESP8266PinISR, CHANGE);
+
+  //Led blink slow
+  S20StatusLed.blink(600,600);
+
+  //WiFiManager
+  WiFiManager wifiManager;
+  wifiManager.setAPCallback(configModeCallback);
+
+  if (!wifiManager.autoConnect()) {
+    Serial.println("WiFiManager: failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("WiFiManager: connected...");
+  S20StatusLed.blinkOff();
+
+  //LED off
+  digitalWrite(PIN_GREENLED, HIGH);
+
+  //OTA
+  //Port defaults to 8266
+  ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("SmartDrankKast");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("ArduinoOTA: Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("ArduinoOTA: started...");
+  Serial.println("ArduinoOTA: Ready");
+  Serial.print("ArduinoOTA: IP address: ");
+  Serial.println(WiFi.localIP());
+
+  BlynkTijd.setInterval(1000L, BlynkTimerHandler);
+  BlynkRtc.begin();
+}
+
+BLYNK_WRITE(V0)
+{
+  BlynkTijdsDuur = param.asInt(); // assigning incoming value from pin V1 to a variable
+  // You can also use:
+  // String i = param.asStr();
+  // double d = param.asDouble();
+  Serial.print("Blynk: Slider value is: ");
+  Serial.println(BlynkTijdsDuur);
+}
+
+BLYNK_WRITE(V1) {
+  TimeInputParam t(param);
+
+  if (t.hasStartTime())
+  {
+    Serial.println(String("Blynk Tijdstip: ") +
+                   t.getStartHour() + ":" +
+                   t.getStartMinute() + ":" +
+                   t.getStartSecond());
+    BlynkUur = t.getStartHour();
+    BlynkMinuut = t.getStartMinute();
+    BlynkSeconde = t.getStartSecond();
+
+    //Activeer timer indien tijdstip en tijdsduur zijn gezet.
+    if(BlynkTijdsDuur) {
+      BlynkTimerArmed = true;
+    } else {
+      BlynkTimerArmed = false;
+    }
+  } else {
+    Serial.println("BlynkTijdstip: onjuiste parameter");
+  }
+}
+
+BLYNK_WRITE(V2) {
+  BlynkCountDown = (60 * 60) * BlynkTijdsDuur;
+  BlynkTimerArmed = false;
+}
+
 void loop() {
-  // OTA request handling
   ArduinoOTA.handle();
-
-  //  WebServer requests handling
-  server.handleClient();
-
-   //  feed de DOG :) 
-   customWatchdog = millis();
-
-  //**** Normal Skecth code here ... 
-  
-
+  Blynk.run();
+  S20StatusLed.blink();
 }

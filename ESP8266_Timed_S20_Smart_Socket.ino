67c02b9a31c742019547bb2b153943fc819c0e34
Bart Theeuwes <barthe@Bart-PC.lan>
2018-01-12 20:16:24 +0100

    * fixed led, date and button

--- ESP8266_Timed_S20_Smart_Socket.ino
+++ ESP8266_Timed_S20_Smart_Socket.ino
@@ -15,12 +15,17 @@
 //Hardware
 #include <TimedBlink.h>
 
+//Time
+#include <time.h>
+#include <NtpClientLib.h>
+boolean syncEventTriggered = false; // True if a time even has been triggered
+NTPSyncEvent_t ntpEvent; // Last triggered event
+
+//Blynk
 #include <BlynkSimpleEsp8266.h>
 #include <TimeLib.h>
-#include <WidgetRTC.h>
 BlynkTimer  BlynkTijd;
 WidgetLED   BlynkStatusLed(V4);
-WidgetRTC   BlynkRtc;
 static int  BlynkTijdsDuur = 1;
 static int  BlynkUur = 18;
 static int  BlynkMinuut = 0;
@@ -29,70 +34,146 @@ static long BlynkCountDown = 0;
 static bool BlynkTimerArmed = false;
 
 //S20
-#include <eBtn.h>
+#include <OneButton.h>
 #define PIN_BUTTON       0
 #define PIN_BLUELEDRELAY 12
 #define PIN_GREENLED     13
-eBtn S20Button = eBtn(PIN_BUTTON);
+OneButton S20Button(PIN_BUTTON, true);
 
-TimedBlink S20StatusLed(PIN_GREENLED);
+//LED Blink
+#include <Ticker.h>
+Ticker S20LedTicker;
+enum LedBlinkType {
+  LED_BLINK_OFF = 0,
+  LED_BLINK_ON,
+  LED_BLINK_SLOW,
+  LED_BLINK_FAST,
+  LED_BLINK_1,
+  LED_BLINK_2,
+  LED_BLINK_3,
+  LED_BLINK_4
+} LedBlinkState = LED_BLINK_OFF;
 
-char auth[] = "YourAuthToken";
+char auth[] = "youkey";
+
+void S20LedTickerHandler() {
+  static int LedTick = 0;
+  bool LedState;
+  
+  LedTick++;
+  if(LedTick >= 10)
+    LedTick = 0;
+   
+  switch(LedBlinkState) {
+    case LED_BLINK_OFF:
+      LedState = false;
+    break;
+    case LED_BLINK_ON:
+      LedState = true;
+    break;
+    case LED_BLINK_SLOW:
+      if(LedTick / 5)
+        LedState = true;
+      else
+        LedState = false;
+    break;
+    case LED_BLINK_FAST:
+    if(LedTick / 2)
+        LedState = true;
+      else
+        LedState = false;
+    break;
+    case LED_BLINK_1:
+    if(LedTick == 0)
+        LedState = true;
+      else
+        LedState = false;
+    break;
+    case LED_BLINK_2:
+    if((LedTick == 0) || (LedTick == 2))
+        LedState = true;
+      else
+        LedState = false;
+    break;
+    case LED_BLINK_3:
+    if((LedTick == 0) || (LedTick == 2)  || (LedTick == 4))
+        LedState = true;
+      else
+        LedState = false;
+    break;
+    case LED_BLINK_4:
+    if((LedTick == 0) || (LedTick == 2)  || (LedTick == 4) || (LedTick == 6))
+        LedState = true;
+      else
+        LedState = false;
+    break;
+  }
+
+  if(LedState) {
+    digitalWrite(PIN_GREENLED, LOW);
+  } else {
+    digitalWrite(PIN_GREENLED, HIGH);
+  }
+}
 
 void S20ButtonRelease() {
   //S20 Button is ingedrukt, tijd verhogen met 1 uur, max 4 uur.
+  Serial.println("S20: Button Pressed");
   BlynkCountDown += (60 * 60); 
   if(BlynkCountDown > (4 * 60 * 60))
-    BlynkCountDown = 4 * 60 * 60; 
+    BlynkCountDown = 4 * 60 * 60;
 }
 
 void S20ButtonLong() {
+  Serial.println("S20: Button Long Pressed");
   BlynkCountDown = 0;
-}
-
-void ESP8266PinISR(){
-  S20Button.handle();
 }
 
 //gets called when WiFiManager enters configuration mode
 void configModeCallback (WiFiManager *myWiFiManager) {
   Serial.println("WiFiManager: Entered config mode");
   Serial.println(WiFi.softAPIP());
+  
   //if you used auto generated SSID, print it
   Serial.println(myWiFiManager->getConfigPortalSSID());
-  //Led blink fast
-  S20StatusLed.blink(200,200);
+  
+  LedBlinkState = LED_BLINK_FAST;
 }
 
 void BlynkTimerHandler(void) {
   //DEBUG
-  String currentTime = String(hour()) + ":" + minute() + ":" + second();
+  time_t tijd;
+  struct tm *tm;
   String setTime = String(BlynkUur) + ":" + BlynkMinuut + ":" + BlynkSeconde;
 
-  Serial.print("Current time: ");
-  Serial.println(currentTime);
+  if(BlynkCountDown)
+    BlynkCountDown--;
 
   //1. Update LCD
-  String s = String("op tijdstip?: ");
+  String s = String("Tijdklok actief:\t ");
   if(BlynkTimerArmed)
-    s += "ja";
+    s += "Ja";
   else
-    s += "nee";
-  s += "; " + setTime + "bezig: " + BlynkCountDown;
+    s += "Nee";
+  s += "; " + setTime + "\nHoe lang nog aan: " + BlynkCountDown / 60 + " m";
   Blynk.virtualWrite(V3, s);
 
   //2. Controlleer of CountDown actief is
   if(BlynkCountDown) {
     digitalWrite(PIN_BLUELEDRELAY, HIGH);
     BlynkStatusLed.on();
+    Blynk.virtualWrite(V2, HIGH);
   } else {
     digitalWrite(PIN_BLUELEDRELAY, LOW);
     BlynkStatusLed.off();
+    Blynk.virtualWrite(V2, LOW);
   }
 
   //3. Controlleer of er een Tijdschakeling actief is
   if(BlynkTimerArmed) {
-    if((hour() == BlynkUur) && (minute() == BlynkMinuut) && (second() >= BlynkSeconde)) {
+    tijd = NTP.getTime();
+    tm = localtime (&tijd);
+    if((tm->tm_hour == BlynkUur) && (tm->tm_min == BlynkMinuut) && (tm->tm_sec >= BlynkSeconde)) {
       BlynkCountDown = (60 * 60) * BlynkTijdsDuur;
       BlynkTimerArmed = false;
     }
@@ -104,28 +185,45 @@ void BlynkTimerHandler(void) {
   n += BlynkCountDown % 3600 ? 1 : 0;
   switch(n) {
     case 0:
-      S20StatusLed.blinkOff();
+      if(BlynkTimerArmed)
+        LedBlinkState = LED_BLINK_ON;
+      else
+        LedBlinkState = LED_BLINK_OFF;
     break;
     case 1:
-      S20StatusLed.blink(50, 900);
+      LedBlinkState = LED_BLINK_1;
     break;
     case 2:
-      S20StatusLed.blink(50, 450);
+      LedBlinkState = LED_BLINK_2;
     break;
     case 3:
-      S20StatusLed.blink(50, 300);
+      LedBlinkState = LED_BLINK_3;
     break;
     case 4:
     default:
-      S20StatusLed.blink(50, 200);
+      LedBlinkState = LED_BLINK_4;
     break;
+  }
+}
+
+void processSyncEvent(NTPSyncEvent_t ntpEvent) {
+  if (ntpEvent) {
+    Serial.print("NTP: Time Sync error: ");
+    if (ntpEvent == noResponse)
+      Serial.println("Server not reachable");
+    else if (ntpEvent == invalidAddress)
+      Serial.println("Invalid NTP server address");
+  }
+  else {
+    Serial.print("NTP: Got NTP time: ");
+    Serial.println(NTP.getTimeDateString(NTP.getLastNTPSync()));
   }
 }
 
 void setup() {
   //Serial
   Serial.begin(115200);
-  Serial.println("Booting SmartDrankKast 1.0");
+  Serial.println("Start SmartDrankKast 1.0");
 
   //S20 Hardware
   pinMode(PIN_GREENLED, OUTPUT);
@@ -134,15 +232,14 @@ void setup() {
   digitalWrite(PIN_BLUELEDRELAY, LOW);
   pinMode(PIN_BUTTON, INPUT);
 
-  //S20Button.on("press",pressFunc);
-  //S20Button.on("hold",holdFunc);
-  S20Button.on("release",S20ButtonRelease);
-  S20Button.on("long",S20ButtonLong);
-  //setting the interrupt on btn pin to react on change state
-  attachInterrupt(PIN_BUTTON, ESP8266PinISR, CHANGE);
+  //Button
+  //attachInterrupt(PIN_BUTTON, ESP8266PinISR, FALLING);
+  S20Button.attachClick(S20ButtonRelease);
+  S20Button.attachLongPressStart(S20ButtonLong);
 
-  //Led blink slow
-  S20StatusLed.blink(600,600);
+  //Led blink
+  LedBlinkState = LED_BLINK_SLOW;
+  S20LedTicker.attach(0.1, S20LedTickerHandler);
 
   //WiFiManager
   WiFiManager wifiManager;
@@ -157,10 +254,6 @@ void setup() {
 
   //if you get here you have connected to the WiFi
   Serial.println("WiFiManager: connected...");
-  S20StatusLed.blinkOff();
-
-  //LED off
-  digitalWrite(PIN_GREENLED, HIGH);
 
   //OTA
   //Port defaults to 8266
@@ -192,17 +285,24 @@ void setup() {
   Serial.print("ArduinoOTA: IP address: ");
   Serial.println(WiFi.localIP());
 
+  Blynk.config(auth);
+  Blynk.connect();
+
   BlynkTijd.setInterval(1000L, BlynkTimerHandler);
-  BlynkRtc.begin();
+
+  NTP.onNTPSyncEvent([](NTPSyncEvent_t event) {
+    ntpEvent = event;
+    syncEventTriggered = true;
+  });
+  
+  NTP.begin("nl.pool.ntp.org", 1, true);
+  NTP.setInterval(63);
 }
 
 BLYNK_WRITE(V0)
 {
   BlynkTijdsDuur = param.asInt(); // assigning incoming value from pin V1 to a variable
-  // You can also use:
-  // String i = param.asStr();
-  // double d = param.asDouble();
-  Serial.print("Blynk: Slider value is: ");
+  Serial.print("Blynk: Slider waarde is: ");
   Serial.println(BlynkTijdsDuur);
 }
 
@@ -215,6 +315,9 @@ BLYNK_WRITE(V1) {
                    t.getStartHour() + ":" +
                    t.getStartMinute() + ":" +
                    t.getStartSecond());
+    Serial.print("Blynk NTP Tijd");
+    Serial.println(NTP.getTimeDateString());
+    
     BlynkUur = t.getStartHour();
     BlynkMinuut = t.getStartMinute();
     BlynkSeconde = t.getStartSecond();
@@ -231,12 +334,28 @@ BLYNK_WRITE(V1) {
 }
 
 BLYNK_WRITE(V2) {
-  BlynkCountDown = (60 * 60) * BlynkTijdsDuur;
-  BlynkTimerArmed = false;
+  if(param.asInt() == 1)
+  {
+     BlynkCountDown = (60 * 60) * BlynkTijdsDuur;
+  }
+  else
+  {
+     BlynkCountDown = 0;
+  }
+  
+  //BlynkTimerArmed = false;
+  Serial.print("Blynk: BlynkCountDown waarde is: ");
+  Serial.println(BlynkCountDown);
 }
 
 void loop() {
   ArduinoOTA.handle();
   Blynk.run();
-  S20StatusLed.blink();
+  BlynkTijd.run();
+  S20Button.tick();
+
+  if (syncEventTriggered) {
+    processSyncEvent(ntpEvent); 
+    syncEventTriggered = false;
+  }  
 }
